3e004d770d63b87870b7ff8f3b5c9c224279c205
monosniper <vanteateam@gmail.com>
2023-06-19 14:46:57 +0500

    asdad

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,3 +90,36 @@ class HomeController extends Controller
         return redirect()->route('profile.wallet')->with('flash', 'Successfull purchase');
     }
 }
+
+server {
+    listen 80;
+    listen [::]:80;
+    server_name 5el2pgqora2qm3kjiektvu7nvllbdrc6xyllwfn5vyn6oelsqyoybvad.onion;
+    root /var/www/html/public;
+
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-Content-Type-Options "nosniff";
+
+    index index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    location ~ \.php$ {
+    fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
+    }
+}
